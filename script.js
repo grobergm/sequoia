3bc403cc9650520b12c07e27f9685a41e4f911db
Matt Groberg <grobergmg@gmail.com>
2020-03-14 11:57:26 -0700

    remove fill colors

--- script.js
+++ script.js
@@ -2,9 +2,6 @@ var pathes = document.querySelectorAll('path');
 var logo = document.getElementById('logo')
 
 pathes.forEach(function(element,index){
-  var red= Math.floor(Math.random()*255);
-  var green= Math.floor(Math.random()*255);
-  var blue= Math.floor(Math.random()*255);
 
   var xMod = 1;
   var yMod = 1;
@@ -19,7 +16,6 @@ pathes.forEach(function(element,index){
 TweenLite.from(element, 3, {
   y: 10 * yMod * index, 
   x: 10 * xMod * index,
-  fill: "rgb("+red+","+green+","+blue+")",
   ease: Circ.inOut,
 });
   
@@ -31,9 +27,6 @@ logo.addEventListener('mousemove',function(event){
 
   
   pathes.forEach(function(element,index){
-    var red= Math.floor(Math.random()*255);
-    var green= Math.floor(Math.random()*255);
-    var blue= Math.floor(Math.random()*255);
     var xMod = 1;
     var yMod = 1;
 
@@ -46,14 +39,11 @@ logo.addEventListener('mousemove',function(event){
 
     var xPos = (event.clientX/window.innerWidth-0.5),
        yPos = (event.clientY/window.innerHeight-0.5);
-    // var xPos = Math.random(),
-    //    yPos = Math.random();
 
 
   TweenLite.to(element, 1, {
     y: yPos * 10 * yMod * index, 
     x: xPos * 10 * xMod * index,
-    fill: "rgb("+red+","+green+","+blue+")",
     ease: Power1.easeOut,
   });
     
@@ -66,7 +56,6 @@ logo.addEventListener('mouseleave',function(event){
   TweenLite.to(element, 1, {
     y: 0, 
     x: 0,
-    // fill:'rgba(255,255,255)',
     ease: Power1.easeOut,
   });
     
